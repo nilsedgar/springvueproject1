a5b2fdb17245f0cb3e79ea8adb7da37006dd49ca
Nils Jacobsen <nilsedgarjacobsen@gmail.com>
2020-12-05 08:58:02 +0100

    full upload

---
+++ src/main/java/com/example/springvueproject/SpringvueprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springvueproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvueprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringvueprojectApplication.class, args);
+    }
+
+}
